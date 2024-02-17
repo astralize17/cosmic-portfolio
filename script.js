a1833289d302dc266b587b42c78c36138724ccac
elco5414 <elco5414@colorado.edu>
2024-02-17 13:39:48 -0700

    more pages

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const sliders = document.querySelectorAll('.slider');
 
+sliders.forEach(slider => {
+    let currentSlide = 0;
+    const slides = slider.querySelectorAll('img');
+    const totalSlides = slides.length;
+
+    slider.addEventListener('click', () => {
+        currentSlide++;
+        if (currentSlide >= totalSlides) {
+            currentSlide = 0;
+        }
+        updateSlide();
+    });
+
+    function updateSlide() {
+        slides.forEach((slide, index) => {
+            slide.style.opacity = index === currentSlide ? '1' : '0'; // Show current slide, hide others
+        });
+    }
+});
